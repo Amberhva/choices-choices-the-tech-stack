48b25c3ed102390d926febadca048fbc006e243d
Amberhva <amber.van.der.reijden@hva.nl>
2023-11-01 02:28:17 +0100

    Update

---
+++ choices-choices-the-tech-stack/src/components/Contact.vue
@@ -0,0 +1,32 @@
+<script></script>
+
+<template>
+    <section class="contact section" id="experience">
+        <div class="contact-container container grid">
+            <div class="contact-data">
+                <div class="contact-text">
+                    <h2>We houden van korte lijntjes, jij toch ook?</h2>
+                    <h2>Kom met ons in gesprek!</h2>
+                    <span class="send-us">Stuur ons een mail</span>
+                </div>
+                <p class="mail-text"><strong>info@spatwater.nl</strong></p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.contact {
+    padding: 4rem 3rem;
+    height: auto;
+    background-color: var(--lg-bg);
+}
+.contact-text {
+    font-size: 0.8em;
+    color: var(--darkblue);
+}
+
+.contact-data {
+    color: var(--spat);
+}
+</style>
